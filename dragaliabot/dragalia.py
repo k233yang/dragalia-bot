c43f78b60142d6e7a5752ae9fa20823893afd393
Kenneth Yang <k233yang@uwaterloo.ca>
2021-10-09 16:55:38 -0400

    basic functions

--- dragaliabot/dragalia.py
+++ dragaliabot/dragalia.py
@@ -11,19 +11,24 @@ bot = discord.Client()
 
 @bot.event
 async def on_ready():
-	guild_count = 0
+        guild_count = 0
 
-	# LOOPS THROUGH ALL THE GUILD / SERVERS THAT THE BOT IS ASSOCIATED WITH.
-	for guild in bot.guilds:
-		print(f"- {guild.id} (name: {guild.name})")
+        # LOOPS THROUGH ALL THE GUILD / SERVERS THAT THE BOT IS ASSOCIATED WITH.
+        for guild in bot.guilds:
+                print(f"- {guild.id} (name: {guild.name})")
+                guild_count = guild_count + 1
 
-		guild_count = guild_count + 1
-
-	print("DragaliaBot is in " + str(guild_count) + " guilds.")
+        print("DragaliaBot is in " + str(guild_count) + " guilds.")
 
 @bot.event
 async def on_message(message):
-	if message.content == "hello":
-		await message.channel.send("hey dirtbag")
+        length = len(message.content)
+        #if message.content == "hello":
+        #        await message.channel.send("hey dirtbag")
+        if message.content[0:3] == "dl!":
+                if message.content[3:length] == " help":
+                        await message.channel.send("stfu")
+                elif message.content[3:length] == " hello":
+                        await message.channel.send("smh")
 
 bot.run(DISCORD_TOKEN)
